d5e72b944ef8138969784bfcf3d07c316bfc7def
Chip Nguyen <chipn@nvidia.com>
2018-11-07 10:22:26 -0800

    visualize

--- examples/pytorch/wlm/wlm_azkaban.py
+++ examples/pytorch/wlm/wlm_azkaban.py
@@ -10,39 +10,27 @@ script_to_run = "examples/pytorch/wlm/start_wlm_azkaban.sh"
 # specify the tunable parameters as cmd arguments and their possible ranges
 params_to_tune = {
   "--model": {
-    "type": "values", "values": ["LSTM", "GRU", "QRNN"]
+    "type": "values", "values": ["RNN_TANH", "RNN_RELU", "LSTM", "GRU"]
   },
   "--emsize": {
-    "type": "values", "values": [128, 160, 196, 224, 256, 320, 384, 448, 512, 576, 640, 704, 768, 896, 1024]
+    "type": "values", "values": [256, 512, 650, 1024, 1504, 2048]
   },
   "--nlayers": {
     "type": "values", "values": [1, 2, 3]
   },
   "--lr": {
-    "type": "values", "values": [5, 10, 12, 15, 18, 19, 20, 22, 25, 30, 40],
+    "type": "values", "values": [10, 20, 5, 30, 25, 40],
   },
   "--nhid": {
-    "type": "values", "values": [224, 256, 320, 384, 448, 512, 576, 640, 704, 768, 896, 1024, 1280]
+    "type": "values", "values": [256, 512, 650, 1024, 1504, 2048]
   },
   "--bptt": {
-    "type": "values", "values": [16, 32, 48, 64, 96, 128, 160, 196]
+    "type": "values", "values": [15, 20, 30, 35, 45]
   },
   "--clip": {
     "type": "range", "min": 0.1, "max": 2.0
   },
   "--dropout": {
-    "type": "range", "min": 0.0, "max": 0.9
-  },
-  "--dropouth": {
-    "type": "range", "min": 0.0, "max": 0.9
-  },
-  "--dropoute": {
-    "type": "range", "min": 0.0, "max": 0.9
-  },
-  "--dropouti": {
-    "type": "range", "min": 0.0, "max": 0.9
-  },
-  "--wdrop": {
     "type": "range", "min": 0.0, "max": 0.9
   },
 }
@@ -63,7 +51,7 @@ objective = "minimize"
 # specify backend information and workers configuration
 backend = AzkabanBackend
 backend_params = {
-  "url": "http://10.110.40.104", # URL of your Azkaban UI
+  "url": "http://127.0.0.1", # URL of your Azkaban UI
   "port": "8081", # Azkaban port. You should see Azkaban UI at url:port
   "username": "azkaban",
   "password": "azkaban",
@@ -72,26 +60,14 @@ backend_params = {
   # is done below:
   "workers_config": [
     {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=0"]},
+    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=1"]},
     {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=0"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=0"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=0"]},
     {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=1"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=1"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=1"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=1"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=2"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=2"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=2"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=2"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=3"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=3"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=3"]},
-    {"num_workers": 1, "env_vars": ["CUDA_VISIBLE_DEVICES=3"]},
   ],
 }
 
 # specify search algorithm to use
 search_algorithm = RandomSearch
 search_algorithm_params = {
-  "num_evals": 1000,
+  "num_evals": 8,
 }
